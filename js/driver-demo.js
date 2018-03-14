4a5df9c0320c99343a6e49fa7ab45d332adcbd71
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-03-14 08:23:48 +0100

    Add allow close option

--- js/driver-demo.js
+++ js/driver-demo.js
@@ -132,7 +132,7 @@ document.addEventListener("DOMContentLoaded", function () {
     /////////////////////////////////////////////
     // Form focus examples
     /////////////////////////////////////////////
-    var focusDriver = new Driver({ padding: 0 });
+    var focusDriver = new Driver({padding: 0});
     var inputIds = ['creation-input', 'creation-input-2', 'creation-input-3', 'creation-input-4'];
     inputIds.forEach(function (inputId) {
         // Highlight the section on focus
@@ -318,6 +318,35 @@ document.addEventListener("DOMContentLoaded", function () {
         e.stopPropagation();
         featureIntroductionDriver.start();
     });
+
+    const singleNoClose = new Driver({
+        allowClose: false,
+        position: 'top'
+    });
+
+    singleNoClose.defineSteps([
+        {
+            element: '#single-element-no-close',
+            popover: {
+                title: 'Uh-huh!',
+                description: 'You cannot close by clicking outside'
+            }
+        },
+        {
+            element: '#third-element-introduction',
+            popover: {
+                title: 'Title on Popover',
+                description: 'Body of the popover',
+                position: 'top'
+            }
+        }
+    ]);
+
+    document.querySelector('#run-single-element-no-close')
+        .addEventListener('click', function (e) {
+            e.preventDefault();
+            singleNoClose.start();
+        });
 
     var newURL = location.href.split("?")[0];
     if (newURL !== location.href) {
